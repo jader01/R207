4160390b0ba7556b1e2942c6d5aee4349c33b44c
Jade R-L <119926041+jader01@users.noreply.github.com>
2024-02-20 14:51:43 +0100

    Update sql_tp3.py
    import comment

--- sql_tp3.py
+++ sql_tp3.py
@@ -1,21 +1,24 @@
 import sqlite3 as sql
 import json
 
-con = sql.connect("computer.sqlite")
+con = sql.connect("computer.sqlite") #lien vers la vse de doné
 cur = con.cursor()
 
 cur.execute("SELECT nompc, refcomp FROM machine, composant WHERE EXISTS (SELECT refcomp FROM assemble WHERE assemble.refcomp=Composant.refcomp AND machine.numpc=assemble.numpc)")
+#au dessus ^ la req sql a excuter
 res = cur.fetchall()
 
-# Obtenir les noms des colonnes
-column_names = [description[0] for description in cur.description]
 
-print("<!Doctype html>")
+# Obtenir les noms des colonnes
+column_names = [description[0] for description in cur.description] #pour recup le nom des colones 
+
+print("<!Doctype html>") #debut du code html
 
 print("<style>table{border-collapse:collapse;}td{text-align:center;border:1px solid black;}th{text-align:center;border:1px solid black;}</style>")
+# ^ tout le style du tableau
 
-print("<center>")
-print("<table>")
+print("<center>") #pour centrer le tableau
+print("<table>") #creation tableau
 
 # Générer automatiquement les balises th en fonction des noms de colonnes
 print("<tr>")
