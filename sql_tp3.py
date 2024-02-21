599a903c60d895559ba7a3b1d0c2616312c1caa7
Jade R-L <119926041+jader01@users.noreply.github.com>
2024-02-21 08:58:54 +0100

    Update sql_tp3.py
    commentaire suite

--- sql_tp3.py
+++ sql_tp3.py
@@ -4,7 +4,7 @@ import json
 con = sql.connect("computer.sqlite") #lien vers la vse de doné
 cur = con.cursor()
 
-cur.execute("SELECT nompc, refcomp FROM machine, composant WHERE EXISTS (SELECT refcomp FROM assemble WHERE assemble.refcomp=Composant.refcomp AND machine.numpc=assemble.numpc)")
+cur.execute("SELECT nompc FROM machine WHERE EXISTS (SELECT numpc FROM assemble WHERE EXISTS (SELECT DISTINCT refcomp FROM cartevideo WHERE ramvideo>=8 AND cartevideo.refcomp=assemble.refcomp AND machine.numpc=assemble.numPC))")
 #au dessus ^ la req sql a excuter
 res = cur.fetchall()
 
@@ -14,29 +14,35 @@ column_names = [description[0] for description in cur.description] #pour recup l
 
 print("<!Doctype html>") #debut du code html
 
+print("<html>")
+print("<head>")
+
 print("<style>table{border-collapse:collapse;}td{text-align:center;border:1px solid black;}th{text-align:center;border:1px solid black;}</style>")
 # ^ tout le style du tableau
+
+print("</head>")
 
 print("<center>") #pour centrer le tableau
 print("<table>") #creation tableau
 
 # Générer automatiquement les balises th en fonction des noms de colonnes
-print("<tr>")
-for col_name in column_names:
-    print("")
-    print("<th>", col_name, "</th>")
+print("<tr>") #titre en haut du tableau en gras (nom de colonne)
+for col_name in column_names: #proute toutes les colones
+    #print("")
+    print("<th>", col_name, "</th>") #on affiche le nom des collones 
 print("</tr>")
 
-for row in res:
+for row in res: #pour tout paramètre dans le résultat de la requete 
     print("<tr>")
     for column in row:
-        if column is None:
+        if column is None: #si pas de colone on met un - 
             print("<td>-</td>")
         else:
-            print("<td>" + str(column) + "</td>")
+            print('<td>' + str(column) + "</td> </font>") #sinon 
     print("</tr>")
 
 print("</table>")
 print("</center>")
+print("</html>")
 
 con.close()
